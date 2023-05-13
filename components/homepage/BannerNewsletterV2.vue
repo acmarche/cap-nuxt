c5032eebd4865e31b2b1f88c1a6a0250dca0e88c
jfsenechal <jfsenechal@gmail.com>
2023-05-13 21:10:49 +0200

    add V2

---
+++ components/homepage/BannerNewsletterV2.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex h-16 relative justify-self-end self-end text-white -bottom-5 w-3/5">
+        <div class="w-36 h16 triangle-bottomright"></div>
+        <div class="flex items-center pl-12 px-8 text-lg  bg-blue-banner">
+            Inscrivez-vous à la newsletter
+        </div>
+        <div class="bg-blue-default flex items-center px-5 text-lg">
+            Par ici !
+        </div>
+        <div class="flex-grow bg-blue-banner"></div>
+    </div>
+</template>
+<style>
+.triangle-bottomright {
+    width: 0;
+    height: 0;
+    border-bottom: 4rem solid #9AD7F7;
+    border-left: 4rem solid transparent;
+}
+
+.angle {
+    transform: skew(120deg);
+}
+</style>
+<script setup lang="ts">
+</script>
