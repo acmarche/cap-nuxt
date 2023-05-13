ed7e9a84c3071630c10d5eb60fb5cefb2bbdf2f4
jfsenechal <jfsenechal@gmail.com>
2023-05-13 20:49:11 +0200

    add banner

--- components/homepage/SearchSection.vue
+++ components/homepage/SearchSection.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import FormSearch from "~/components/homepage/FormSearch.vue";
+import BannerNewsletter from "~/components/homepage/BannerNewsletterV2.vue";
 </script>
 <template>
-    <section class="w-full min-h-[45rem] bg-green-400 bg-hero-pattern flex flex-col justify-center items-center">
+    <section class="w-full min-h-[45rem] bg-green-400 bg-hero-pattern flex flex-col justify-center items-center relative">
         <h1 class="pb-6 pt-6 text-center text-4xl text-white font-pathway-bold">
             DÉCOUVREZ VOS ENTREPRISES ET COMMERÇANTS MARCHOIS2
         </h1>
         <FormSearch/>
+        <BannerNewsletter/>
     </section>
 </template>
