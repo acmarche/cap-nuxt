d1d1f1fc10ac2387b19d1d5d17b2e9a70144a356
jfsenechal <jfsenechal@gmail.com>
2023-05-13 19:02:28 +0200

    BannerNewsletter

---
+++ components/homepage/BannerNewsletter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex h-16 bg-blue-banner">
+        <div class="w-1/3 bg-white">Empty</div>
+        <div class="bg-white" id="triangle-bottomright">
+
+        </div>
+        <div class="text-white flex">
+            <div class="flex items-center px-8 text-lg">
+                Inscrivez-vous à la newsletter
+            </div>
+            <div class="bg-blue-default flex items-center px-5 text-lg">
+                Par ici !
+            </div>
+        </div>
+    </div>
+
+</template>
+<style>
+#triangle-bottomright {
+    width: 0;
+    height: 0;
+    border-bottom: 4rem solid #9AD7F7;
+    border-left: 4rem solid transparent;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SearchSection from "~/components/homepage/SearchSection.vue";
+import BannerNewsletter from "~/components/homepage/BannerNewsletter.vue";
 
 definePageMeta({
     layout: 'homelayout',
@@ -10,4 +11,5 @@ definePageMeta({
 <template>
     <SearchSection/>
     <h2 class="text-2xl font-pathway-bold text-blue-default">L'ACTU DES COMMERCANTS</h2>
+    <BannerNewsletter/>
 </template>
