a6e4036d90c2343c054c4dcc606770fe05b928b4
jfsenechal <jfsenechal@gmail.com>
2023-05-13 21:10:37 +0200

    with grid

--- components/homepage/SearchSection.vue
+++ components/homepage/SearchSection.vue
@@ -3,7 +3,8 @@ import FormSearch from "~/components/homepage/FormSearch.vue";
 import BannerNewsletter from "~/components/homepage/BannerNewsletterV2.vue";
 </script>
 <template>
-    <section class="w-full min-h-[45rem] bg-green-400 bg-hero-pattern flex flex-col justify-center items-center relative">
+    <section
+        class="w-full min-h-[45rem] bg-green-400 bg-hero-pattern bg-bottom grid grid-cols-1 grid-rows-[200px_200px_1fr] justify-center items-center relative">
         <h1 class="pb-6 pt-6 text-center text-4xl text-white font-pathway-bold">
             DÉCOUVREZ VOS ENTREPRISES ET COMMERÇANTS MARCHOIS2
         </h1>
