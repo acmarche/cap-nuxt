d9b0195f41f9d0e50f02e6680a6770906ec91fb8
jfsenechal <jfsenechal@gmail.com>
2023-05-14 19:44:47 +0200

    add

---
+++ components/homepage/News.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="h-60 w-full mt-12">
+        <h2 class="text-2xl font-pathway-bold text-blue-default">L'ACTU DES COMMERCANTS</h2>
+        <div>
+
+        </div>
+    </section>
+</template>
