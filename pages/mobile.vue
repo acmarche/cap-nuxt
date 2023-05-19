1db1376ce33de7c3ffa77d1b1ce7e3b811166b39
jfsenechal <jfsenechal@gmail.com>
2023-05-19 21:12:49 +0200

    ease-in-out

--- pages/mobile.vue
+++ pages/mobile.vue
@@ -14,7 +14,7 @@ definePageMeta({
     </a>
     <ul
         :class="mobileMenu ? 'h-[450px]' : 'h-0'"
-        class="overflow-hidden absolute left-0 right-0 top-20 w-full mb-24 bg-white transition-all duration-700 flex flex-col items-center text-lg font-pathway-semi-bold text-grey-dark">
+        class="overflow-hidden absolute left-0 right-0 top-20 w-full mb-24 bg-white transition-all duration-700 ease-in-out flex flex-col items-center text-lg font-pathway-semi-bold text-grey-dark">
       <li v-for="item in items" :key="item.id"
           class="flex flex-col items-center justify-center w-full hover:text-blue-default border-t border-grey-lighter divide-y-4 divide-red-500">
         <a href="#" class="block p-8">{{ item.name }}</a>
