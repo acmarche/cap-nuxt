4838b46cae5f0ee6a66f437a3108cab56abcd956
jfsenechal <jfsenechal@gmail.com>
2023-05-20 20:40:57 +0200

    remove, rename

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,12 +1,12 @@
 <script setup>
-import Items from "@/components/header/Items.vue";
+import TopNavigation from "~/components/header/TopNavigation.vue";
 </script>
 <template>
     <header class="flex justify-end items-center w-full px-3 bg-white shadow-md h-32">
         <a href="/" class="mr-auto self-center">
             <img src="assets/src/resources/wholeLogo.png" alt="logo"/>
         </a>
-        <Items/>
+      <TopNavigation/>
         <a href="/" class="w-6 space-x-6 mr-4">
             <img src="assets/src/resources/iconLogin.png" alt="login"/>
         </a>
---
+++ components/header/TopNavigation.vue
@@ -0,0 +1,11 @@
+<script setup>
+import {items} from "~/composables/menuItemsGet";
+</script>
+<template>
+  <nav class="flex gap-4 items-center text-lg mr-4 font-pathway-regular">
+    <a v-for="item in items" :key="item.id" :href="item.url"
+       class="">
+      {{ item.name }}
+    </a>
+  </nav>
+</template>
