3500bbb59fd87646c29bbe96c9f17a9b7fc6b6c6
jfsenechal <jfsenechal@gmail.com>
2023-05-22 14:16:22 +0200

    clean

--- components/header/TopNavigation.vue
+++ components/header/TopNavigation.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {items} from "~/composables/menuItemsGet";
 
-const {mobileMenu = true} = defineProps(['mobileMenu'])
+const {mobileMenu = false} = defineProps(['mobileMenu'])
 </script>
 <template>
   <nav
@@ -11,7 +11,7 @@ const {mobileMenu = true} = defineProps(['mobileMenu'])
        class="flex flex-col lg:block items-center justify-center w-full lg:w-auto p-4 lg:p-0 hover:text-blue-default border-t-2 lg:border-0 border-grey-lighter">
       {{ item.name }}
     </a>
-    <a href="/" class="w-6 space-x-6 mr-4">
+    <a href="/" class="w-6 space-x-6 lg:mr-4">
       <img src="assets/src/resources/iconLogin.png" alt="login"/>
     </a>
   </nav>
