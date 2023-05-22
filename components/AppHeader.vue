42dc4cd63670aed7157be47f0246849ad9bc4f74
jfsenechal <jfsenechal@gmail.com>
2023-05-22 13:51:49 +0200

    begin header mix

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,14 +2,10 @@
 import TopNavigation from "~/components/header/TopNavigation.vue";
 
 const mobileMenu = ref(false)
-definePageMeta({
-  layout: 'noheaderlayout',
-  title: 'test',
-})
 </script>
 <template>
-  <header class="flex flex-row justify-center lg:justify-end items-center w-full px-3 bg-white shadow-md h-32">
-    <a href="#" @click.prevent="mobileMenu = !mobileMenu" class="flex lg:hidden mr-auto ">
+  <header class=" w-full flex flex-row justify-center lg:justify-end items-center px-3 bg-white shadow-md h-28 lg:h-32">
+    <a href="#" @click.prevent="mobileMenu = !mobileMenu" class="flex self-start pt-3 lg:hidden mr-auto ">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
            class="w-8 h-8">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
@@ -18,7 +14,6 @@ definePageMeta({
     <a href="/" class="mr-auto lg:self-center">
       <img src="assets/src/resources/wholeLogo.png" alt="logo"/>
     </a>
-    <!-- -->
-    <TopNavigation/>
+    <TopNavigation :mobileMenu="mobileMenu"/>
   </header>
 </template>
--- components/header/TopNavigation.vue
+++ components/header/TopNavigation.vue
@@ -1,11 +1,14 @@
 <script setup>
 import {items} from "~/composables/menuItemsGet";
+
+const {mobileMenu = true} = defineProps(['mobileMenu'])
 </script>
 <template>
   <nav
-      class="overflow-hidden bg-white flex flex-col lg:flex-row absolute lg:static left-0 right-0 top-24 w-full lg:w-auto gap-4 items-center text-lg mr-4 font-pathway-regular text-grey-dark">
+      :class="mobileMenu ? 'h-[450px] lg:h-full' : 'h-0 lg:h-full'"
+      class="overflow-hidden z-20 lg:z-auto bg-white flex flex-col lg:flex-row lg:gap-4 absolute lg:static left-0 right-0 top-28 w-full lg:w-auto items-center text-lg mr-4 font-pathway-regular text-grey-dark transition-all duration-700 ease-in-out ">
     <a v-for="item in items" :key="item.id" :href="item.url"
-       class="">
+       class="flex flex-col lg:block items-center justify-center w-full lg:w-auto p-4 lg:p-0 hover:text-blue-default border-t-2 lg:border-0 border-grey-lighter">
       {{ item.name }}
     </a>
     <a href="/" class="w-6 space-x-6 mr-4">
