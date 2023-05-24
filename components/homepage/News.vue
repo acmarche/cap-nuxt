513007177044f14671b8ec54f3d91eb8886da0a3
jfsenechal <jfsenechal@gmail.com>
2023-05-24 21:50:18 +0200

    better for pc

--- components/homepage/News.vue
+++ components/homepage/News.vue
@@ -2,39 +2,39 @@
 import {news} from "~/composables/newsItemsGets"
 </script>
 <template>
-    <section class="w-full mt-12 bg-geometric bg-no-repeat bg-bottom-left">
-        <div class="w-2/3 mx-auto">
-            <h2 class="text-2xl font-pathway-bold text-blue-default mb-3">L'ACTU DES COMMERCANTS</h2>
-            <ul class="grid grid-cols-1 lg:grid-cols-3 gap-2">
-              <li v-for="item in news" :key="item.id" class="h-96">
-                <a :href="item.url" class="block h-full w-full news-block">
-                  <div class="flex flex-col min-h-80">
-                    <img :src="item.img" alt="img" class="h-48 w-auto object-cover">
-                    <div class="flex flex-row gap-2 pt-2 bg-grey-light">
-                      <img src="https://cap.marche.be/media/portrait-1.JPG" alt="team"
-                           class="rounded-full h-6 w-6">
-                      <div class="flex flex-col text-blue-default h-44">
-                        <span>{{ item.date }}</span>
-                        <h4 class="font-pathway-semi-bold text-lg">{{ item.name }}</h4>
-                                    <span>{{ item.excerpt }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-            </ul>
-            <a href="https://cap.marche.be/evenements" class="block ml-auto w-52 mt-2">
-                <div
-                    class="bg-blue-default hover:bg-blue-lighter text-white w-64 pt-2 p-3 text-lg flex flex-row justify-center items-center">
-                    Voir plus d’évenements
-                </div>
-            </a>
+  <section class="w-full mt-8 lg:mt-12 bg-geometric bg-no-repeat bg-bottom-left">
+    <div class="w-full px-5 lg:w-2/3 lg:px-2 mx-auto">
+      <h2 class="text-2xl font-pathway-semi-bold text-blue-default mb-3 leading-8">L'ACTU DES COMMERCANTS</h2>
+      <ul class="grid grid-cols-1 lg:grid-cols-3 gap-2">
+        <li v-for="item in news" :key="item.id" class="">
+          <a :href="item.url" class="block h-full w-full news-block">
+            <div class="flex flex-col">
+              <img :src="item.img" alt="img" class="h-48 w-auto object-cover">
+              <div class="flex flex-row items-center justify-start gap-2 pt-2 px-2 bg-grey-light">
+                <img src="https://cap.marche.be/media/portrait-1.JPG" alt="team"
+                     class="rounded-full h-12 w-12">
+                <h4 class="font-pathway-semi-bold text-lg">{{ item.name }}</h4>
+              </div>
+              <div class="flex flex-col justify-between text-blue-default bg-grey-light py-3 px-4 h-28">
+                <span>{{ item.excerpt }}</span>
+                <span class="text-grey-dark text-base self-end ">{{ item.date }}</span>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+      <a href="https://cap.marche.be/evenements" class="block ml-auto w-52 mt-2">
+        <div
+            class="bg-blue-default hover:bg-blue-lighter text-white w-64 pt-2 p-3 text-lg flex flex-row justify-center items-center">
+          Voir plus d’évenements
         </div>
-    </section>
+      </a>
+    </div>
+  </section>
 </template>
 <style>
 a.news-block:hover {
-    box-shadow: 0px 0px 42px 3px rgba(0, 0, 0, 0.2);
-    -webkit-box-shadow: 0px 0px 42px 3px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 42px 3px rgba(0, 0, 0, 0.2);
+  -webkit-box-shadow: 0 0 42px 3px rgba(0, 0, 0, 0.2);
 }
 </style>
