dd082b012b1b7a132041d64979046aadf3d8fd9d
jfsenechal <jfsenechal@gmail.com>
2023-06-08 16:36:13 +0200

    form search component

--- components/shops/FormSearchShops.vue
+++ components/shops/FormSearchShops.vue
@@ -1,8 +1,20 @@
 <script setup>
 import categoriesGet from "~/composables/categoriesGet";
 
-const categorySelected = ref(0)
-const childSelected = ref(0)
+const propos = defineProps({
+  categorySelected: Number,
+  childSelected: Number
+})
+
+const catSelected = ref(propos.categorySelected)
+const childSelected = ref(propos.childSelected)
+
+const emits = defineEmits(['update:categorySelected', 'update:childSelected'])
+
+function upd() {
+  emits('update:categorySelected', catSelected.value)
+  emits('update:childSelected', childSelected.value)
+}
 
 const {
   pendingCategories,
@@ -10,36 +22,40 @@ const {
   errorCategories
 } = categoriesGet()
 
-const url = computed(() => `http://api.local/bottin/categories/byparent/${categorySelected.value}`)
+const url = computed(() => `http://api.local/bottin/categories/byparent/${catSelected.value}`)
 const {data: childs} = useFetch(url)
 
 </script>
 <template>
-  <form name="search" class="flex flex-row gap-2">
+  <form name="search" class="">
     <template v-if="pendingCategories">
       Loading Events...
     </template>
     <template v-if="errorCategories" class="text-red-600">
       Error {{ errorCategories }}
     </template>
-    <div class="flex flex-col basis-1/2	">
-      <h2 class="text-xl uppercase font-pathway-semi-bold mb-2">TRIER PAR CATÉGORIE</h2>
-      <select name="category" v-if="categories" class="text-grey-e" v-model="categorySelected">
-        <option value="0">Choisir une catégorie</option>
-        <option v-for="category in categories" :value="category.id">
-          {{ category.name }}
-        </option>
-      </select>
-    </div>
 
-    <div class="flex flex-col basis-1/2	">
-      <h2 class="text-xl uppercase font-pathway-semi-bold mb-2">TRIER PAR SOUS-CATÉGORIE</h2>
-      <select name="child" class="text-grey-e" v-model="childSelected">
-        <option value="0">Choisir une catégorie</option>
-        <option v-if="childs" v-for="child in childs" :value="child.id">
-          {{ child.name }}
-        </option>
-      </select>
+    <div class="flex flex-row gap-2">
+      <div class="flex flex-col basis-1/2	">
+        <h2 class="text-xl uppercase font-pathway-semi-bold mb-2">TRIER PAR CATÉGORIE</h2>
+        <select name="category" v-if="categories" class="text-grey-e" v-model="catSelected" @change="upd">
+          <option value="0">Choisir une catégorie</option>
+          <option v-for="category in categories" :value="category.id">
+            {{ category.name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="flex flex-col basis-1/2	">
+        <h2 class="text-xl uppercase font-pathway-semi-bold mb-2">TRIER PAR SOUS-CATÉGORIE</h2>
+        <select name="child" class="text-grey-e" v-model="childSelected" @change="upd">
+          <option value="0">Choisir une catégorie</option>
+          <option v-if="childs" v-for="child in childs" :value="child.id">
+            {{ child.name }}
+          </option>
+        </select>
+      </div>
+
     </div>
 
     <div class="flex flex-row mt-6">
