0a896e51adea188c6a7a3e67491a43f4505fa88e
jfsenechal <jfsenechal@gmail.com>
2023-08-28 13:38:34 +0200

    banner on photo

--- components/shops/FicheItem.vue
+++ components/shops/FicheItem.vue
@@ -6,7 +6,7 @@ const shopImage = computed(() => {
     if (propos.fiche.cap.profileMediaPath !== null) {
       return `https://cap.marche.be/${propos.fiche.cap.profileMediaPath}`
     }
-    if (propos.fiche.cap.images.length > 0) {
+    if (propos.fiche.cap.images && propos.fiche.cap.images.length > 0) {
       return `https://cap.marche.be/${propos.fiche.cap.images[0]}`
     }
   }
@@ -16,11 +16,38 @@ const shopImage = computed(() => {
   return 'https://cap.marche.be/media/-716383_w1000.jpg'
 })
 
+const shopImageOver = computed(() => {
+  return 'https://cap.marche.be/media/02.jpg'
+  if (propos.fiche.cap) {
+    if (propos.fiche.cap.images && propos.fiche.cap.images.length > 1) {
+      return `https://cap.marche.be/${propos.fiche.cap.images[0]}`
+    }
+    if (propos.fiche.cap.images && propos.fiche.cap.images.length > 0) {
+      return `https://cap.marche.be/${propos.fiche.cap.images[0]}`
+    }
+  }
+  if (propos.fiche.photos && propos.fiche.photos.length > 0) {
+    return propos.fiche.photos[0]
+  }
+  return 'https://cap.marche.be/media/02.jpg'
+})
+
 </script>
 <template>
-  <NuxtLink :to="`shop/${fiche.id}`">
-    <img :src="shopImage" class="w-[410px] h-[244px]"
-         alt="">
+  <NuxtLink :to="`shop/${fiche.id}`" class="block h-64 w-96 ">
+    <div class="grid [grid-template-areas:block] h-64 w-96  group">
+      <div
+          class="[grid-area:block] h-64 w-96  bg-center bg-no-repeat bg-cover"
+          :style="`background-image:url(${shopImage})`">
+      </div>
+      <div
+          class="block group [grid-area:block] h-64 w-96 opacity-0 bg-center bg-no-repeat bg-cover group-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+          :style="`background-image:url(${shopImageOver})`">
+      </div>
+      <p class="w-96 self-end rounded-b-lg bg-black/50 p-2 text-center text-white [grid-area:block] opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+        {{ propos.fiche.cap ? propos.fiche.cap.commercialWordTitle : 'Beautiful Turkey' }}
+      </p>
+    </div>
     <div class="flex flex-col bg-grey-white border border-grey-white p-8">
       <h3 class="text-xl text-grey-shop uppercase font-pathway-semi-bold">{{ propos.fiche.societe }}</h3>
       <h4 class="text-lg text-grey-shop">{{ propos.fiche.cap ? propos.fiche.cap.commercialWordTitle : 'non' }}</h4>
--- pages/tous-les-commerces.vue
+++ pages/tous-les-commerces.vue
@@ -10,30 +10,42 @@ definePageMeta({
 
 const categorySelected = ref(0)
 const childSelected = ref(0)
-const fiches = ref([])
 
-watch(childSelected, async (newchildSelected, oldQuestion) => {
-  if (newchildSelected > 0) {
-    console.log(newchildSelected)
-    const url = computed(() => `http://api.local/bottin/fiches/rubrique/${newchildSelected}`)
-    console.log(url.value)
-    const {data: fiches2} = await useFetch(url.value)
-    fiches.value = fiches2.value
-  }
-})
+const {
+  data,
+  error,
+  pending
+} = await useFetch(() => `http://api.local/bottin/fiches/cap/rubrique/${childSelected.value}`, {
+  onRequest({request, options}) {
+  },
+  onRequestError({request, options, error}) {
+    console.log(error)
+  },
+  onResponse({request, response, options}) {
+    console.log(123)
+  },
+  onResponseError({request, response, options}) {
+    console.log(response)
+  },
+});
+
 </script>
 <template>
   <section class="w-full">
     <p>categorySelected: {{ categorySelected }}</p>
     <p>childSelected: {{ childSelected }}</p>
-
     <div class="bg-shops-form text-white bg-blue-search p-28">
       <h1 class="text-4xl mb-8 font-pathway-semi-bold">NOS COMMERCES</h1>
       <FormSearchShops v-model:categorySelected="categorySelected" v-model:childSelected="childSelected"/>
     </div>
 
     <LegendIcones/>
-    <ListResult :fiches="fiches"/>
+
+    <span v-if="pending">Loading...</span>
+    <div v-else-if="data">
+      <ListResult :fiches="data"/>
+    </div>
+    <span v-else-if="error">Error: {{ error }}</span>
 
   </section>
 </template>
