17afa7c3e087c41269d79028cdb7b1d291c8f3ad
jfsenechal <jfsenechal@gmail.com>
2023-09-12 14:14:36 +0200

    add NuxtErrorBoundary

--- app.vue
+++ app.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
 useHead({
-  titleTemplate:(titleChunk)=>{
-    return titleChunk ? `${titleChunk} - 'Cap sur Marche'`:'Cap sur Marche';
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - 'Cap sur Marche'` : 'Cap sur Marche';
   }
 })
 </script>
 <template>
   <NuxtLayout>
-    <NuxtLoadingIndicator color="#3a87a4ff" />
-    <NuxtPage/>
+    <NuxtLoadingIndicator color="#3a87a4ff"/>
+    <NuxtErrorBoundary>
+      <template #error="{error}">
+        <div>
+          <p>
+            Oh no, something broke when loading the page
+            <code>{{ error }}</code>
+          </p>
+          <p>
+            <button
+                class="hover:cursor-pointer"
+                @click="clearError(error)">
+              Go to homepage
+            </button>
+          </p>
+        </div>
+      </template>
+      <NuxtPage/>
+    </NuxtErrorBoundary>
   </NuxtLayout>
 </template>
