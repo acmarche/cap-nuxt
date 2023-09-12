8101f6ef091f3d4a853e4eb2859c7110d596bfe0
jfsenechal <jfsenechal@gmail.com>
2023-09-12 14:14:48 +0200

    clean

--- layouts/homelayout.vue
+++ layouts/homelayout.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="app">
-        <AppHeader/>
-      <main>
-            <slot/>
-        </main>
-        <AppFooter/>
-    </div>
+  <div id="app">
+    <AppHeader/>
+    <main>
+      <slot/>
+    </main>
+    <AppFooter/>
+  </div>
 </template>
