bacbfa1162c1c241879531c29850e94138167c2e
jfsenechal <jfsenechal@gmail.com>
2023-09-13 14:31:02 +0200

    clean

--- components/secteurs/HeaderSearch.vue
+++ components/secteurs/HeaderSearch.vue
@@ -1,21 +1,11 @@
 <script setup lang="ts">
-import {IconChevronRight} from '@tabler/icons-vue';
-
 const {category} = defineProps<{
   category: { type: Object, required: true },
 }>()
-
-const pathComputed = computed(() => category.path.map((element) => {
-  if (element.id === 511 || element.id === 610) { //remove economie, commerces et entreprises
-    return null;
-  }
-  return element;
-}).filter((element) => element !== null));
-
 </script>
 <template>
   <header class="bg-search-header bg-blue-search p-12">
-    <h1 itemprop="headline" class="text-3xl text-white font-pathway-semi-bold uppercase mb-3">
+    <h1 itemprop="headline" class="text-3xl text-white font-pathway-semi-bold uppercase mb-4">
       {{ category.name }} à Marche-en-Famenne</h1>
     <ShopBreadcumb :path="category.path" text-color="text-white" text-color-hover="hover:text-green-default"/>
     <p class="text-white m-3 prose lg:prose-xl">
