59542aa51ca986d76a5e49f92734b76a924e404e
jfsenechal <jfsenechal@gmail.com>
2023-09-13 14:32:14 +0200

    nicer error

--- app.vue
+++ app.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import {IconBug} from '@tabler/icons-vue';
+
 useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - Cap sur Marche` : 'Cap sur Marche';
@@ -10,8 +12,9 @@ useHead({
     <NuxtLoadingIndicator color="#3a87a4ff"/>
     <NuxtErrorBoundary>
       <template #error="{error}">
-        <div>
-          <p>
+        <div class="flex flex-row gap-2 items-center justify-center my-5">
+          <IconBug class="h-8 w-8 flex-shrink-0" aria-hidden="true"/>
+          <p class="text-red-600">
             Oh no, something broke when loading the page
             <code>{{ error }}</code>
           </p>
