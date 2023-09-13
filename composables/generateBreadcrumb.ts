8e2d300aae11a9c3c56fb8a1311e3206d957a288
jfsenechal <jfsenechal@gmail.com>
2023-09-13 14:34:37 +0200

    clean

--- composables/generateBreadcrumb.ts
+++ composables/generateBreadcrumb.ts
@@ -3,11 +3,8 @@ export default (path: Array<Object>): Array<String> => {
     let root = '/secteurs'
     let breadcrumb:Array<String> = []
 
-    console.log(path)
-
     breadcrumb[0] = `${root}/${path[0].slug}`
     breadcrumb[1] = `${breadcrumb[0]}/${path[1].slug}`
 
-    console.log(breadcrumb)
     return breadcrumb
 }
