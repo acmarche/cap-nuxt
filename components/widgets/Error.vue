2f21813962c6600cbe28315e0687d30635765cbf
jfsenechal <jfsenechal@gmail.com>
2023-09-13 14:32:02 +0200

    html

--- components/widgets/Error.vue
+++ components/widgets/Error.vue
@@ -6,5 +6,8 @@ const {error} = defineProps<{
 </script>
 
 <template>
-      <span v-else-if="error" class="text-red-600"><IconBug class="h-8 w-8 flex-shrink-0" aria-hidden="true"/> Error: {{ error }}</span>
+      <div v-else-if="error" class="text-red-600">
+        <IconBug class="h-8 w-8 flex-shrink-0" aria-hidden="true"/>
+        Error: {{ error }}
+      </div>
 </template>
