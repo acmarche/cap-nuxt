30ce001d12e295ac0c968130da3fa365a7d992e2
jfsenechal <jfsenechal@gmail.com>
2023-09-14 10:43:52 +0200

    better css

--- components/homepage/News.vue
+++ components/homepage/News.vue
@@ -5,30 +5,26 @@ import {news} from "~/composables/newsItemsGets"
   <section class="w-full mt-8 bg-geometric22 bg-no-repeat bg-bottom-left relative">
     <HomepageBannerNewsletter/>
     <div class="w-full px-5 lg:w-2/3 lg:px-2 mx-auto">
-      <h2 class="text-2xl font-pathway-semi-bold text-blue-default mb-3 leading-8">L'ACTU DES COMMERCANTS</h2>
-      <ul class="grid grid-cols-1 lg:grid-cols-3 gap-2">
-        <li v-for="item in news" :key="item.id" class="">
-          <NuxtLink :to="item.url" class="block h-full w-full news-block">
-            <div class="flex flex-col">
-              <img :src="item.img" alt="img" class="h-60 w-auto object-cover">
-              <div class="flex flex-row items-center justify-start gap-2 pt-2 px-2 bg-grey-light">
-                <img src="https://cap.marche.be/media/portrait-1.JPG" alt="team"
-                     class="rounded-full h-12 w-12">
-                <h4 class="font-pathway-semi-bold text-lg">{{ item.name }}</h4>
-              </div>
-              <div class="flex flex-col justify-between text-blue-default bg-grey-light py-3 px-4 h-28">
-                <span>{{ item.excerpt }}</span>
-                <span class="text-grey-dark text-base self-end ">{{ item.date }}</span>
-              </div>
+      <h2 class="text-2xl font-pathway-semi-bold text-blue-default py-4 block leading-8">L'ACTU DES COMMERCANTS</h2>
+      <ul class="grid grid-cols-1 lg:grid-cols-3 gap-2 items-start">
+        <li v-for="item in news" :key="item.id" class="h-full w-full news-block">
+          <NuxtLink :to="item.url" class="flex flex-col ">
+            <img :src="item.img" alt="img" class="h-60 w-auto object-cover">
+            <div class="flex flex-row items-center justify-start gap-2 pt-2 px-2 bg-grey-light h-24">
+              <img src="https://cap.marche.be/media/portrait-1.JPG" alt="team"
+                   class="rounded-full h-12 w-12">
+              <h4 class="font-pathway-semi-bold text-lg line-clamp-2">{{ item.name }}</h4>
+            </div>
+            <div class="flex flex-col justify-between text-blue-default bg-grey-light py-3 px-4 h-32">
+              <span class="line-clamp-2">{{ item.excerpt }}</span>
+              <span class="text-grey-dark text-base self-end ">{{ item.date }}</span>
             </div>
           </NuxtLink>
         </li>
       </ul>
-      <NuxtLink to="/evenements" class="block ml-auto w-64 mt-2">
-        <div
-            class="bg-blue-default hover:bg-blue-lighter text-white pt-2 p-3 text-lg flex flex-row justify-center items-center">
-          Voir plus d’évenements
-        </div>
+      <NuxtLink to="/evenements"
+                class="flex flex-row justify-center items-center ml-auto w-64 mt-2 bg-blue-default hover:bg-blue-lighter text-white pt-2 p-3 text-lg ">
+        Voir plus d’évenements
       </NuxtLink>
     </div>
   </section>
@@ -38,7 +34,7 @@ import {news} from "~/composables/newsItemsGets"
   background-image: url('/images/bg-map-diagonal.png');
 }
 
-a.news-block:hover {
+.news-block a:hover {
   box-shadow: 0 0 42px 3px rgba(0, 0, 0, 0.2);
   -webkit-box-shadow: 0 0 42px 3px rgba(0, 0, 0, 0.2);
 }
--- components/homepage/SearchSection.vue
+++ components/homepage/SearchSection.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
     <section
-        class="z-10 overflow-hidden w-full bg-green-400 bg-hero-pattern bg-bottom flex flex-col relative lg:grid lg:grid-cols-2 lg:grid-rows-[200px_200px_1fr] lg:justify-center lg:items-center lg:min-h-[45rem] ">
+        class="z-10 overflow-hidden w-full bg-blue-lighter-banner bg-hero-pattern bg-bottom flex flex-col relative lg:grid lg:grid-cols-2 lg:grid-rows-[200px_200px_1fr] lg:justify-center lg:items-center lg:min-h-[45rem] ">
       <h1 class="pb-6 pt-28 lg:pt-6 text-center text-2xl lg:text-4xl text-white font-pathway-semi-bold lg:col-span-2">
         DÉCOUVREZ VOS ENTREPRISES ET COMMERÇANTS MARCHOIS
       </h1>
