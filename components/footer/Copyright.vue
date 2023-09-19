c47bcc35e8eab0b3c17af33f860991cafd60de3d
jfsenechal <jfsenechal@gmail.com>
2023-09-19 16:07:47 +0200

    padding

--- components/footer/Copyright.vue
+++ components/footer/Copyright.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-grey-footer text-white w-full py-5">
+  <div class="bg-grey-footer text-white w-full p-5">
     2016 - Réalisation et gestion du site par
     <NuxtLink to="http://cst.be/" target="_blank" title="App &amp; Web - Digital Agency"> AppWeb</NuxtLink>
     - Copyright © 2016 - Marche-en-Famenne. Tous droits réservés <span class="flex items-center lg:justify-end">
